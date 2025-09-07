35cc72ac1c266a17d2a3be9699460e2bad61658a
sadeen abukhadra <sadeenmahmoud752@gmail.com>
2025-09-07 15:40:50 +0300

    task3

--- src/main/java/org/example/dataprocessor/enums/OutputType.java
+++
@@ -1,9 +0,0 @@
-package org.example.dataprocessor.enums;
-
-public enum OutputType {
-    /** Print "Result = <value>" to stdout */
-    CONSOLE,
-
-    /** Write "Result = <value>" to target/result.txt (overwrite, create dirs) */
-    TEXT_FILE
-}
